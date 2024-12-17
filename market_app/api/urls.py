a3cecce982c3b0103865713c0108cc32999c1248
wavyStructures <anja_schwab@gmx.de>
2024-12-17 10:44:04 +0100

    market_single_view

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import markets_view
+from .views import markets_view, market_single_view
 
 urlpatterns = [
     path('', markets_view),
+    path('<int:pk>/', market_single_view),   
 ]
+
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -2,17 +2,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import MarketSerializer
+from market_app.models import Market
+
 
 @api_view(['GET', 'POST'])
 def markets_view(request):
-    
-    
-    
     if request.method == 'GET':
-        return Response({"message": "Hello, world!"})
+        markets = Market.objects.all()
+        serializer = MarketSerializer(markets, many=True)
+        return Response(serializer.data)
     if request.method == 'POST':
-        try:
-            msg = request.data['message']
-            return Response({"message": msg}, status=status.HTTP_404_NOT_FOUND)
-        except:
-            return Response({"message": "No message provided"}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = MarketSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def market_single_view(request, pk):
+    if request.method == 'GET':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        return Response(serializer.data)
